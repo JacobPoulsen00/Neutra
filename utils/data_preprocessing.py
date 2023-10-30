39c06a8b576abd869ff271ae56067130b6b9329e
JacobPoulsen00 <jacobhogstedt@gmail.comm>
2023-10-30 12:31:40 +0100

    Added webapp and routes

--- utils/data_preprocessing.py
+++ utils/data_preprocessing.py
@@ -1,51 +1,61 @@
 import re
-import os
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 
-def download_nltk_data_if_needed(packages):
-    for package in packages:
-        try:
-            nltk.data.find(package)
-        except LookupError:
-            nltk.download(package.split("/")[-1])
+HTML_TAG_PATTERN = re.compile('<.*?>')
 
-# Specify the NLTK data you need
 required_nltk_packages = [
     'tokenizers/punkt',
     'corpora/wordnet',
     'corpora/stopwords'
 ]
 
-# Call the function to download data if needed
-download_nltk_data_if_needed(required_nltk_packages)
+def download_nltk_data_if_needed(packages):
+    """
+    Download specified NLTK data packages if they aren't already present.
+    """
+    for package in packages:
+        try:
+            nltk.data.find(package)
+        except LookupError:
+            nltk.download(package.split("/")[-1])
 
+download_nltk_data_if_needed(required_nltk_packages)
 lemmatizer = WordNetLemmatizer()
-stop_words = set(stopwords.words('english'))
+stopword_set = set(stopwords.words('english'))
 
 def remove_html_tags(text):
-    # Remove html tags from a string
-    clean = re.compile('<.*?>')
-    return re.sub(clean, '', text)
+    """
+    Remove HTML tags from a string.
+    """
+    return HTML_TAG_PATTERN.sub('', text)
 
 def tokenize(text):
-    # Tokenize text
+    """
+    Tokenize text.
+    """
     return word_tokenize(text)
 
-def remove_stopwords(tokens):
-    # Remove stopwords from a list of tokens
-    return [i for i in tokens if not i.lower() in stop_words]
+def remove_stopwords(tokens, stopword_set):
+    """
+    Remove stopwords from a list of tokens.
+    """
+    return [token for token in tokens if token.lower() not in stopword_set]
 
-def lemmatize(tokens):
-    # Lemmatize a list of tokens
+def lemmatize(tokens, lemmatizer):
+    """
+    Lemmatize a list of tokens.
+    """
     return [lemmatizer.lemmatize(token) for token in tokens]
 
 def preprocess_text(text):
-    # Main function to clean and preprocess text
+    """
+    Main function to clean and preprocess text.
+    """
     text = remove_html_tags(text)
     tokens = tokenize(text)
-    tokens = remove_stopwords(tokens)
-    tokens = lemmatize(tokens)
+    tokens = remove_stopwords(tokens, stopword_set)
+    tokens = lemmatize(tokens, lemmatizer)
     return ' '.join(tokens)
---
+++ webapp/static/js/script.js
@@ -0,0 +1,4 @@
+// Sample JS code (if you need any dynamic actions)
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Web app initialized!');
+});
